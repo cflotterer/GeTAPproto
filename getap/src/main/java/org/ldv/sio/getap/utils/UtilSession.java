dc84e7d9fc9af4822b30b35157c2ef3b3abb6f18
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2012-03-19 23:15:34 +0100

    Ajout de la posiibilité de mettre une valeur d'année scolaire dans la session de l'utilisateur (par exemple : anneeScolaire = "2011-2012")

--- getap/src/main/java/org/ldv/sio/getap/utils/UtilSession.java
+++ getap/src/main/java/org/ldv/sio/getap/utils/UtilSession.java
@@ -7,18 +7,29 @@ import org.springframework.web.context.request.RequestContextHolder;
 /**
  * Un Helper de gestion de l'utilisateur dans sa session HTTP
  * 
- * @author kpu
+ * @author sio-slam melun
  * 
  */
 
 public class UtilSession {
 	static public void setUserInSession(User user) {
-		RequestContextHolder.currentRequestAttributes().setAttribute("user",
-				user, RequestAttributes.SCOPE_SESSION);
+		RequestContextHolder.currentRequestAttributes().setAttribute("user", user,
+		    RequestAttributes.SCOPE_SESSION);
 	}
 
 	static public User getUserInSession() {
-		return (User) RequestContextHolder.currentRequestAttributes()
-				.getAttribute("user", RequestAttributes.SCOPE_SESSION);
+		return (User) RequestContextHolder.currentRequestAttributes().getAttribute(
+		    "user", RequestAttributes.SCOPE_SESSION);
 	}
+
+	static public void setAnneeScolaireInSession(String anneeScolaire) {
+		RequestContextHolder.currentRequestAttributes().setAttribute(
+		    "anneeScolaire", anneeScolaire, RequestAttributes.SCOPE_SESSION);
+	}
+
+	static public String getAnneeScolaireInSession() {
+		return (String) RequestContextHolder.currentRequestAttributes()
+		    .getAttribute("anneeScolaire", RequestAttributes.SCOPE_SESSION);
+	}
+
 }
