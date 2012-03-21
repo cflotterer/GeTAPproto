57bce46c4cb3fc5a117d091ef4208762c9d6cd2d
Frederic Varni <frederic.varni@vinci-melun.org>
2012-03-21 08:24:01 +0100

    Commentaires sur les propriétés de la classe et son nom.

--- getap/src/main/java/org/ldv/sio/getap/app/DemandeConsoTempsAccPers.java
+++ getap/src/main/java/org/ldv/sio/getap/app/DemandeConsoTempsAccPers.java
@@ -2,142 +2,220 @@ package org.ldv.sio.getap.app;
 
 import java.sql.Date;
 
+/**
+ * Demande de validation d'un temps d'accompagnement personnalisé
+ * 
+ * La classe ne devrait-elle pas s'appeler DdeValidTempsAccPers ou quelque chose
+ * comme ça ? L'élève demande à faire valider un temps d'accompagnement passé,
+ * pas à consommer un temps futur.
+ * 
+ * @author fred
+ * 
+ */
+
 public class DemandeConsoTempsAccPers {
-	private Long id;
-	private String anneeScolaire;
-	private java.sql.Date dateAction;
-	private Integer minutes;
-	private User prof;
-	private AccPersonalise accPers;
-	private Long idEleve;
-	private int etat;
+    /**
+     * Identifiant de la DCTAP
+     */
+    private Long id;
+    /**
+     * Année scolaire de la demande, par exemple "2011-2012"
+     */
+    private String anneeScolaire;
+    /**
+     * Date de réalisation de l'accompagnement
+     * 
+     */
+    private java.sql.Date dateAction;
+    /**
+     * Nombre de minutes d'accompagnement personnalisé à valider
+     */
+    private Integer minutes;
+    /**
+     * Professeur ayant assuré l'accompagnement personnalisé
+     */
+    private User prof;
+    /**
+     * Nature de l'accompagnement personnalisé associé à la demande
+     */
+    private AccPersonalise accPers;
+    /**
+     * Identifiant de l'élève ayant réalisé l'accompagnement personnalisé
+     * Pourquoi pas un User plutôt qu'un long ?????? Incohérent et compliqué à
+     * souhaits
+     */
+    private Long idEleve;
+    /**
+     * Etat actuel de la demande de validation. Valeurs admissibles :
+     * <ul>
+     * <li>0 - demande créée par l'élève</li>
+     * <li>1 - demande modifiée par le professeur</li>
+     * <li>2 - demande validée par le professeur</li>
+     * <li>3 - demande refusée par le professeur</li>
+     * <li>4 - demande détruite par l'élève</li>
+     * </ul>
+     */
+    private int etat;
 
-	public DemandeConsoTempsAccPers() {
+    /**
+     * constructeur par défaut
+     */
+    public DemandeConsoTempsAccPers() {
 
-	}
+    }
 
-	public DemandeConsoTempsAccPers(Long id, String anneeScolaire, Date date,
-	    Integer minutes, User prof, AccPersonalise accPers, Long idEleve, int etat) {
-		super();
-		this.id = id;
-		this.anneeScolaire = anneeScolaire;
-		this.dateAction = date;
-		this.minutes = minutes;
-		this.prof = prof;
-		this.accPers = accPers;
-		this.idEleve = idEleve;
-		this.etat = etat;
-	}
+    /**
+     * Constructeur permettant de créer une demande complète.
+     * 
+     * @param id
+     *            identifiant de la demande, est-ce une bonne idée ? En général
+     *            c'est une valeur fournie par la couche de persistance
+     * @param anneeScolaire
+     * @param date
+     * @param minutes
+     * @param prof
+     * @param accPers
+     * @param idEleve
+     * @param etat
+     */
+    public DemandeConsoTempsAccPers(Long id, String anneeScolaire, Date date,
+            Integer minutes, User prof, AccPersonalise accPers, Long idEleve,
+            int etat) {
+        super();
+        this.id = id;
+        this.anneeScolaire = anneeScolaire;
+        this.dateAction = date;
+        this.minutes = minutes;
+        this.prof = prof;
+        this.accPers = accPers;
+        this.idEleve = idEleve;
+        this.etat = etat;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getAnneeScolaire() {
-		return anneeScolaire;
-	}
+    public String getAnneeScolaire() {
+        return anneeScolaire;
+    }
 
-	public void setAnneeScolaire(String anneeScolaire) {
-		this.anneeScolaire = anneeScolaire;
-	}
+    public void setAnneeScolaire(String anneeScolaire) {
+        this.anneeScolaire = anneeScolaire;
+    }
 
-	public java.sql.Date getDateAction() {
-		return dateAction;
-	}
+    public java.sql.Date getDateAction() {
+        return dateAction;
+    }
 
-	public void setDateAction(java.sql.Date date) {
-		this.dateAction = date;
-	}
+    public void setDateAction(java.sql.Date date) {
+        this.dateAction = date;
+    }
 
-	public Integer getMinutes() {
-		return minutes;
-	}
+    public Integer getMinutes() {
+        return minutes;
+    }
 
-	public void setMinutes(Integer minutes) {
-		this.minutes = minutes;
-	}
+    public void setMinutes(Integer minutes) {
+        this.minutes = minutes;
+    }
 
-	public User getProf() {
-		return prof;
-	}
+    public User getProf() {
+        return prof;
+    }
 
-	public void setProf(User prof) {
-		this.prof = prof;
-	}
+    public void setProf(User prof) {
+        this.prof = prof;
+    }
 
-	public AccPersonalise getAccPers() {
-		return accPers;
-	}
+    public AccPersonalise getAccPers() {
+        return accPers;
+    }
 
-	public void setAccPers(AccPersonalise accPers) {
-		this.accPers = accPers;
-	}
+    public void setAccPers(AccPersonalise accPers) {
+        this.accPers = accPers;
+    }
 
-	public Long getIdEleve() {
-		return idEleve;
-	}
+    public Long getIdEleve() {
+        return idEleve;
+    }
 
-	public void setIdEleve(Long idEleve) {
-		this.idEleve = idEleve;
-	}
+    public void setIdEleve(Long idEleve) {
+        this.idEleve = idEleve;
+    }
 
-	public int getEtat() {
-		return etat;
-	}
+    public int getEtat() {
+        return etat;
+    }
 
-	public void setEtat(int etat) {
-		this.etat = etat;
-	}
+    /**
+     * Permet de modifier l'état de la demande
+     * 
+     * @param etat
+     *            prend ses valeur dans :
+     *            <ul>
+     *            <li>0 - demande créée par l'élève</li>
+     *            <li>1 - demande modifiée par le professeur</li>
+     *            <li>2 - demande validée par le professeur</li>
+     *            <li>3 - demande refusée par le professeur</li>
+     *            <li>4 - demande détruite par l'élève</li>
+     *            </ul>
+     */
+    public void setEtat(int etat) {
+        // TODO vérifier que la transition vers le nouvel état est bien
+        // autorisée
+        this.etat = etat;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-		    + ((anneeScolaire == null) ? 0 : anneeScolaire.hashCode());
-		result = prime * result
-		    + ((dateAction == null) ? 0 : dateAction.hashCode());
-		result = prime * result + ((idEleve == null) ? 0 : idEleve.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((anneeScolaire == null) ? 0 : anneeScolaire.hashCode());
+        result = prime * result
+                + ((dateAction == null) ? 0 : dateAction.hashCode());
+        result = prime * result + ((idEleve == null) ? 0 : idEleve.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DemandeConsoTempsAccPers other = (DemandeConsoTempsAccPers) obj;
-		if (anneeScolaire == null) {
-			if (other.anneeScolaire != null)
-				return false;
-		} else if (!anneeScolaire.equals(other.anneeScolaire))
-			return false;
-		if (dateAction == null) {
-			if (other.dateAction != null)
-				return false;
-		} else if (!dateAction.equals(other.dateAction))
-			return false;
-		if (idEleve == null) {
-			if (other.idEleve != null)
-				return false;
-		} else if (!idEleve.equals(other.idEleve))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DemandeConsoTempsAccPers other = (DemandeConsoTempsAccPers) obj;
+        if (anneeScolaire == null) {
+            if (other.anneeScolaire != null)
+                return false;
+        } else if (!anneeScolaire.equals(other.anneeScolaire))
+            return false;
+        if (dateAction == null) {
+            if (other.dateAction != null)
+                return false;
+        } else if (!dateAction.equals(other.dateAction))
+            return false;
+        if (idEleve == null) {
+            if (other.idEleve != null)
+                return false;
+        } else if (!idEleve.equals(other.idEleve))
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "DemandeConsoTempsAccPers [id=" + id + ", anneeScolaire="
-		    + anneeScolaire + ", dateAction=" + dateAction + ", minutes=" + minutes
-		    + ", prof=" + prof + ", accPers=" + accPers + ", idEleve=" + idEleve
-		    + ", etat=" + etat + "]";
-	}
+    @Override
+    public String toString() {
+        return "DemandeConsoTempsAccPers [id=" + id + ", anneeScolaire="
+                + anneeScolaire + ", dateAction=" + dateAction + ", minutes="
+                + minutes + ", prof=" + prof + ", accPers=" + accPers
+                + ", idEleve=" + idEleve + ", etat=" + etat + "]";
+    }
 
 }
